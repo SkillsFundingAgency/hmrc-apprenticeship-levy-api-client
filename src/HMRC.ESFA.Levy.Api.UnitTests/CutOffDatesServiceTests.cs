71d24d5acc53df26f88ef0560579ac33c465b3fb
Arvinth Seran <arvinth.seran@gmail.com>
2017-10-03 15:25:01 +0100

    esssage=added tests

---
+++ src/HMRC.ESFA.Levy.Api.UnitTests/CutOffDatesServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using HMRC.ESFA.Levy.Api.Client.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HMRC.ESFA.Levy.Api.Types;
+using NUnit.Framework;
+
+namespace HMRC.ESFA.Levy.Api.UnitTests
+{
+    
+    [TestFixture]
+    public class CutOffDatesServiceTests
+    {
+        private CutoffDatesService _cutoffdateservice;
+
+        [SetUp]
+        public void Init()
+        {
+            _cutoffdateservice = new CutoffDatesService();
+        }
+
+        [TestCase(1, 5, 2017)]
+        [TestCase(2, 6, 2017)]
+        [TestCase(3, 7, 2017)]
+        [TestCase(4, 8, 2017)]
+        [TestCase(5, 9, 2017)]
+        [TestCase(6, 10, 2017)]
+        [TestCase(7, 11, 2017)]
+        [TestCase(8, 12, 2017)]
+        [TestCase(9, 1, 2018)]
+        [TestCase(10, 2, 2018)]
+        [TestCase(11, 3, 2018)]
+        [TestCase(12, 4, 2018)]
+        public void ShouldReturnCorrectSubmissionCutOffDate(short taxmonth, int expectedmonth, int expectedyear)
+        {
+            var payrollPeriod = new PayrollPeriod { Year = "17-18", Month = taxmonth };
+            var dateforsubmission = _cutoffdateservice.GetDateTimeForSubmissionCutoff(payrollPeriod);
+            Assert.AreEqual(new DateTime(expectedyear, expectedmonth, 1).ToString("MM/YYYY"), dateforsubmission.ToString("MM/YYYY"));
+        }
+
+        [TestCase(1, 5, 2017)]
+        [TestCase(2, 6, 2017)]
+        [TestCase(3, 7, 2017)]
+        [TestCase(4, 8, 2017)]
+        [TestCase(5, 9, 2017)]
+        [TestCase(6, 10, 2017)]
+        [TestCase(7, 11, 2017)]
+        [TestCase(8, 12, 2017)]
+        [TestCase(9, 1, 2018)]
+        [TestCase(10, 2, 2018)]
+        [TestCase(11, 3, 2018)]
+        [TestCase(12, 4, 2018)]
+        public void ShouldReturnCorrectProcessingCutOffDate(short taxmonth, int expectedmonth, int expectedyear)
+        {
+            var payrollPeriod = new PayrollPeriod { Year = "17-18", Month = taxmonth };
+            var dateforprocessing = _cutoffdateservice.GetDateTimeForProcessingCutoff(payrollPeriod);
+            Assert.AreEqual(new DateTime(expectedyear, expectedmonth, 1).ToString("MM/YYYY"), dateforprocessing.ToString("MM/YYYY"));
+        }
+    }
+}
--- src/HMRC.ESFA.Levy.Api.UnitTests/PaymentStatusTest.cs
+++ src/HMRC.ESFA.Levy.Api.UnitTests/PaymentStatusTest.cs
@@ -70,6 +70,48 @@ namespace HMRC.ESFA.Levy.Api.UnitTests
             });
         }
 
+        
+        [TestCase(24, 9, 2017)]
+        [TestCase(24, 10, 2017)]
+        public void ShouldHaveZeroOrOneLatestPaymentPerMonth(int day, int month, int year)
+        {
+            var today = new DateTime(year, month, day);
+            var declarations = new List<Declaration>();
+            declarations.Add(new Declaration
+            {
+                Id = "month7latestentry",
+                SubmissionTime = new DateTime(today.Year, today.Month - 1, 18, 00, 00, 00, DateTimeKind.Utc),
+                PayrollPeriod = new PayrollPeriod { Month = GetPayrollMonth(today.Month - 2), Year = GetPayollYear(today) }
+            });
+            declarations.Add(new Declaration
+            {
+                Id = "month6latestentry",
+                SubmissionTime = new DateTime(today.Year, today.Month - 2, 18, 00, 00, 00, DateTimeKind.Utc),
+                PayrollPeriod = new PayrollPeriod { Month = GetPayrollMonth(today.Month - 3), Year = GetPayollYear(today) }
+            });
+            declarations.Add(new Declaration
+            {
+                Id = "month8latestentry",
+                SubmissionTime = new DateTime(today.Year, today.Month - 4, 24, 00, 00, 00, DateTimeKind.Utc),
+                PayrollPeriod = new PayrollPeriod { Month = GetPayrollMonth(today.Month - 1), Year = GetPayollYear(today) }
+            });
+            declarations.Add(new Declaration
+            {
+                Id = "month4latestentry",
+                SubmissionTime = new DateTime(today.Year, today.Month - 4, 18, 00, 00, 00, DateTimeKind.Utc),
+                PayrollPeriod = new PayrollPeriod { Month = GetPayrollMonth(today.Month - 5), Year = GetPayollYear(today) }
+            });
+            var declarationsPostProcessed = _processor.ProcessDeclarationsByPayrollPeriod(declarations, today);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(LevyDeclarationPaymentStatus.LatestPayment, declarationsPostProcessed.Single(x => x.Id == "month7latestentry").LevyDeclarationPaymentStatus, $"Check declaration with id month7latestentry");
+                Assert.AreEqual(LevyDeclarationPaymentStatus.LatestPayment, declarationsPostProcessed.Single(x => x.Id == "month6latestentry").LevyDeclarationPaymentStatus, $"Check declaration with id month6latestentry");
+                Assert.AreEqual(LevyDeclarationPaymentStatus.LatestPayment, declarationsPostProcessed.Single(x => x.Id == "month8latestentry").LevyDeclarationPaymentStatus, $"Check declaration with id month8latestentry");
+                Assert.AreEqual(LevyDeclarationPaymentStatus.LatestPayment, declarationsPostProcessed.Single(x => x.Id == "month4latestentry").LevyDeclarationPaymentStatus, $"Check declaration with id month4latestentry");
+            });
+        }
+
+
         public short GetPayrollMonth(int month)
         {
             var taxmonth = month - 3;
