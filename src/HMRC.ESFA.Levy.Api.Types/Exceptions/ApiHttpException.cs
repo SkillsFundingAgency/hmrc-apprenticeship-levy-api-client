ffafc7fa93f5000b318ebeaa105bf810f91410c9
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2021-09-27 16:27:16 +0100

    Initial code for port to net standard 2

--- src/HMRC.ESFA.Levy.Api.Types/Exceptions/ApiHttpException.cs
+++ src/HMRC.ESFA.Levy.Api.Types/Exceptions/ApiHttpException.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿//using Opw.HttpExceptions;
+using Boxed.AspNetCore;
+using System;
 using System.Web;
 
 namespace HMRC.ESFA.Levy.Api.Types.Exceptions
 {
     public class ApiHttpException : HttpException
     {
-        public int HttpCode => GetHttpCode();
+        //public int HttpCode => GetHttpCode();
+        public int HttpCode => this.StatusCode;
+
         public string ResourceUri { get; set; }
 
         public string ResponseBody { get; set; }
